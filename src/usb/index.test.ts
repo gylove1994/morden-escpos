48a8e95358057d118b757e49fbb95d9c15824beb
Geng Yan <gylove1994@acgsteps.com>
2025-12-08 17:19:54 +0800

    feat: add USBAdapter tests and update tsconfig exclusions
    - Introduced a new test file for USBAdapter with various test cases for finding printers, creating adapter instances, and testing adapter methods.
    - Updated tsconfig.json to exclude test files with the pattern '*.test.ts' in addition to the existing 'test' directory exclusion.

--- src/usb/index.test.ts
+++ src/usb/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import USBAdapter from '../usb/index';
+import USBAdapter from './index';
 
 describe('uSBAdapter', () => {
   describe('findPrinter', () => {
